42215e2c833001096b1c08adf8c5826edeaf16b4
Clara Nascimento <clara.nasc13@gmail.com>
2026-06-10 12:31:58 -0300

    feat: initialize Prisma Client with PostgreSQL adapter and add User controller methods

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,14 +1,26 @@
 import { Request, Response } from "express"
+import { prisma } from "../prisma"
 
 class UsersController {
   async index(request: Request, response: Response) {
     return response.json()
   }
 
+  //criando usuário
   async create(request: Request, response: Response) {
+    const {name, email} = request.body
+
+    //método para criar um usuário utilizando o prisma
+    await prisma.user.create({ data:{
+      name,
+      email,
+    }
+    })
+    
     return response.status(201).json()
   }
 
+  //buscando todos os usuários
   async show(request: Request, response: Response) {
     return response.json()
   }
